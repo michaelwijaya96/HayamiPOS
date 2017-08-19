6ea08a996ff6178c252a38b06f84f4d6252001ff
Alvin Oktavianus <agentsam1995@gmail.com>
2017-08-19 18:09:50 +0700

    Create connection string

---
+++ PosAsp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PosAsp.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        [Required, MaxLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
